2a6a8877db166a6557ca3ab2697d22644729107d
Samuel <stotino2000@yahoo.com>
2020-08-18 17:01:49 -0700

    checking reading capability from database

--- inc/functions.php
+++ inc/functions.php
@@ -1,2 +1,16 @@
 <?php
 //application functions
+function get_project_list(){
+    include 'inc/connection.php';
+
+    try{
+        //$sql = "SELECT * FROM projects";
+        //$result = $db->prepare($sql);
+        //$result->execute();
+
+        return $db->query('SELECT project_id, title, category FROM projects');
+    } catch (Exception $e){
+        echo "Error!: " . $e->getMessage() . "</br>";
+        return array();
+    }
+}
