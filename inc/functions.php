36f6fd8bca88cbe5fb69cf1ac417d2d3eb0b7030
Samuel <stotino2000@yahoo.com>
2020-08-24 17:09:53 -0700

    create function to add tasks to db

--- inc/functions.php
+++ inc/functions.php
@@ -47,3 +47,23 @@ function get_task_list(){
         return array();
     }
 }
+
+function add_task($project_id, $title, $date, $time){
+    include 'inc/connection.php';
+
+    $sql = 'INSERT INTO tasks(project_id, title, date, time) VALUES(?, ?, ?, ?)';
+
+    try{
+        $results = $db->prepare($sql);
+        $results->bindValue(1, $project_id, PDO::PARAM_INT);
+        $results->bindValue(2, $title, PDO::PARAM_STR);
+        $results->bindValue(2, $date, PDO::PARAM_STR);
+        $results->bindValue(2, $time, PDO::PARAM_INT);
+        $results->execute();
+    } catch (Exception $e){
+        echo "Error!: " . $e->getMessage() . "<br />";
+        return false;
+    }
+
+    return true;
+}
